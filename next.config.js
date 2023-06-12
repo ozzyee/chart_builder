adc603ba4114818a67a202cf161f2f74e999506f
ozzyee <oearle10@gmail.com>
2023-06-12 20:33:53 +0100

    fix: editor

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const withCSS = require("@zeit/next-css");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
+  }
 }
 
 module.exports = nextConfig
